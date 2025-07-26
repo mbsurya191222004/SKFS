33cb13c51146bfeb8042708e8d29c7f03c6c8862
SURYA <mbsurya19122004@gmail.com>
2025-07-26 16:31:55 +0530

    minor

--- detection.py
+++ detection.py
@@ -1,13 +1,12 @@
 import time
 import cv2 as cv
 from deepface import DeepFace
-
+import ctypes
 
 #TypeError: recognize() missing 1 required positional argument: 'frame'
 
 #vars
 face = "image.png"
-
 
 #dnn detection model init
 modelFile = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
@@ -18,7 +17,6 @@ model_meanSubValues = (104, 177, 123)
 
 #CV video init
 cam = cv.VideoCapture(0)
-
 
 #exceptions
 class NoFrameException(Exception):
@@ -36,7 +34,27 @@ class InterruptedException(Exception):
 
 
 #functions
+def storeFace():
+    OK , frame = cam.read()
 
+    if not OK:
+        raise NoFrameException
+        
+    if cv.waitKey(1) & 0xFF == ord('m'):
+        raise InterruptedException
+    
+    height , width = frame.shape[0:2]
+    
+    detections = detectFaces(frame)
+
+    for i in range(detections.shape[2]):
+        confidence = detections[0,0,i,2]
+        if confidence > 0.5:
+            box = detections[0,0,i,3:7]*[width,height,width,height]
+            x1, y1, x2, y2 = box.astype("int")
+            x1, y1, x2, y2 = x1-50, y1-50, x2+50, y2+50
+            cropped=frame[x1:x2,y1:y2]
+        cv.imwrite("image.png",cropped)
 
 def detectFaces(img):
     blob=cv.dnn.blobFromImage(img,1,model_res,model_meanSubValues)
@@ -47,13 +65,13 @@ def detectFaces(img):
 def markDetections(detections,frame,frameWidth,frameHeight):
     for i in range(detections.shape[2]):
         confidence = detections[0,0,i,2]
-        if confidence > 0.5:
+        if confidence > 0.3:
             box = detections[0,0,i,3:7]*[frameWidth,frameHeight,frameWidth,frameHeight]
             x1, y1, x2, y2 = box.astype("int")
-            x1, y1, x2, y2 = x1-10, y1-10, x2+10, y2+10
+            x1, y1, x2, y2 = x1-20, y1-20, x2+20, y2+20
             cv.rectangle(frame,(x1, y1),(x2, y2) ,(0,0,0),3)
             cropped=frame[x1:x2,y1:y2]
-            verification = DeepFace.verify(img1_path = "image.png", img2_path = cropped, enforce_detection = False , model_name = "ArcFace")
+            verification = DeepFace.verify(img1_path = "image.png", img2_path = cropped, enforce_detection = False , model_name = "ArcFace",detector_backend='mtcnn')
             distance = verification['distance']
             threshold = 0.68
 
@@ -61,6 +79,7 @@ def markDetections(detections,frame,frameWidth,frameHeight):
                 print(True)
             else:
                 print(False)
+                
             return frame
         print("no face")
         return frame
@@ -75,6 +94,8 @@ def startFrames():
         if cv.waitKey(1) & 0xFF == ord('m'):
             raise InterruptedException
         
+        
+        
         height , width = frame.shape[0:2]
 
         detections = detectFaces(frame)
@@ -84,7 +105,7 @@ def startFrames():
         
         cv.imshow("frames",frame)
 
-        time.sleep(1)
+        time.sleep(0.0167)
         
 
 def main():
@@ -94,7 +115,7 @@ def main():
         print(e)
 
 def main2():
-    pass
+    storeFace()
 
 if __name__=="__main__":
     main()
